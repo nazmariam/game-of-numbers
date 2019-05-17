b640d45effb9ab96f1568a663f3f48b2aecc881e
nazmariam <fedirko.mary@gmail.com>
2019-05-17 11:51:46 +0300

    Updates

---
+++ precache-manifest.f0108dcc98a264fb7187402e17c890aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd1fda0dcfe2334f206559ffd6fcecf",
+    "url": "/game-of-numbers/index.html"
+  },
+  {
+    "revision": "ff86a2c4439c36358a30",
+    "url": "/game-of-numbers/static/css/main.1defd2b2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/game-of-numbers/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ff86a2c4439c36358a30",
+    "url": "/game-of-numbers/static/js/main.53247dcd.chunk.js"
+  },
+  {
+    "revision": "6770d3ff94c5e39fc988",
+    "url": "/game-of-numbers/static/js/runtime~main.7a351a09.js"
+  }
+]);

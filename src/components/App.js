d13a7a22d4fa7a2465f5bcd23e5f2b4863f0997b
nazmariam <fedirko.mary@gmail.com>
2019-05-17 11:42:56 +0300

    almost done

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,34 @@
 import React, { Component } from "react";
-
-import "./App.css";
+import { addToStorage, rotate45, match } from "../utils/helpers.js";
 import Field from "./field.js";
-
-const reverseY = (matrix) =>{
-    let reversed=[];
-    for(let i=0; i<4; i++){
-        let row =[];
-        for(let j=0; j<4; j++){
-            row.push(matrix[3-i][j])
-        }
-        reversed.push(row)
-    }
-    return reversed
-};
-
-const rotate45 = (matrix) =>{
-    let reversed=[];
-    for(let i=0; i<4; i++){
-        let row =[];
-        for(let j=0; j<4; j++){
-            row.push(matrix[3-j][i])
-        }
-        reversed.push(row)
-    }
-    return reversed
-};
 
 class App extends Component {
   state = {
-    cells: [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-    ],
-      latestCoordinates:{}
+    cells: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+    latestCoordinates: {},
+    gameOver: false,
+    score: 0
   };
   size = 4;
 
   componentDidMount() {
-    let {cells} = this.state;
+    let { cells } = this.state;
     let cells1 = this.makeRandomCell(cells);
     this.setState(this.makeRandomCell(cells1));
     document.addEventListener("keydown", this.handleKeyDown.bind(this));
+  }
+  setBestScore() {
+    let finishScore = this.state.score;
+    let bestFromStorage = JSON.parse(localStorage.getItem("score"));
+    if (finishScore > bestFromStorage) {
+      addToStorage(finishScore, "score");
+    }
+  }
+  ifGamesOver(arr) {
+    if (this.isGameOver(arr)) {
+      this.setBestScore();
+      this.setState({ gameOver: true });
+    }
   }
   makeRandomCell(data) {
     let arr = data.slice();
@@ -51,8 +36,9 @@ class App extends Component {
     if (this.availableCells(arr).length) {
       let randomNumber = Math.floor(Math.random() * rt.length);
       arr[rt[randomNumber].i][rt[randomNumber].j] = Math.random() < 0.9 ? 2 : 4;
-      this.setState({latestCoordinates:{i:rt[randomNumber].i,j:rt[randomNumber].j}})
-
+      this.setState({
+        latestCoordinates: { i: rt[randomNumber].i, j: rt[randomNumber].j }
+      });
     }
     return arr;
   }
@@ -71,112 +57,150 @@ class App extends Component {
     }
     return avCells;
   }
+  areMatches(arr) {
+    let cells = arr.slice();
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        return (
+          match(cells, i - 1, j, i, j) ||
+          match(cells, i, j - 1, i, j) ||
+          match(cells, i, j + 1, i, j) ||
+          match(cells, i + 1, j, i, j)
+        );
+      }
+    }
+  }
+  isGameOver(arr) {
+    return (
+      !this.areMatches(this.state.cells) && !this.availableCells(arr).length
+    );
+  }
   handleKeyDown(event) {
     const up = 38;
     const right = 39;
     const down = 40;
     const left = 37;
-    let {cells} = this.state;
+    let { cells } = this.state;
     if (event.keyCode === up) {
       let newState = this.moveUp(cells);
-      this.setState(newState)
+      this.setState(newState);
     } else if (event.keyCode === right) {
       let newState = this.moveRight(cells);
-      this.setState({cells:newState})
+      this.setState({ cells: newState });
     } else if (event.keyCode === down) {
       let newState = this.moveDown(cells);
-      this.setState({cells:newState})
+      this.setState({ cells: newState });
     } else if (event.keyCode === left) {
       let newState = this.moveLeft(cells);
-      this.setState({cells:newState})
+      this.setState({ cells: newState });
     }
   }
-  checkStep = (arr, i,j) =>{
-    let moved;
-      if (arr[i][j] !== 0 && arr[i - 1][j] === 0) {
-        arr[i - 1][j] = arr[i][j];
-        arr[i][j] = 0;
+  checkStep = (arr, i, j, moved) => {
+    if (arr[i][j] !== 0 && arr[i - 1][j] === 0) {
+      arr[i - 1][j] = arr[i][j];
+      arr[i][j] = 0;
+      moved = true;
+      if (i >= 2) {
+        this.checkStep(arr, i - 1, j, moved);
         moved = true;
-        if(i>=2){
-          this.checkStep(arr,i-1,j);
+        if (i === 3) {
+          this.checkStep(arr, i - 2, j, moved);
           moved = true;
-          if(i===3){
-            this.checkStep(arr,i-2,j);
-            moved = true;
-          }
         }
-      } else if ((arr[i][j] === arr[i - 1][j])) {
-        arr[i - 1][j] = arr[i][j] + arr[i - 1][j];
-        arr[i][j] = 0;
-        moved = true;
       }
+    } else if (arr[i][j] === arr[i - 1][j]) {
+      arr[i - 1][j] = arr[i][j] + arr[i - 1][j];
+      let score = this.state.score + arr[i][j];
+      this.setState({ score });
+      arr[i][j] = 0;
+      moved = true;
+    }
 
-    return {arr,moved};
+    return { arr, moved };
   };
-  moveUp(data){
+  moveUp(data) {
     let arr = data.slice();
+    let result,
+      moved = false;
+    for (let j = 0; j < this.size; j++) {
+      for (let i = 1; i < this.size; i++) {
+        result = this.checkStep(arr, i, j, moved);
+      }
+    }
+    arr = result.arr;
+    moved = result.moved;
+    this.ifGamesOver(arr);
+    return moved ? this.makeRandomCell(arr) : arr;
+  }
+  moveDown(data) {
+    let arr = data.slice();
+    let arr2 = rotate45(rotate45(arr));
     let moved = false;
-      for(let j = 0; j<this.size; j++){
-          for (let i = 1; i < this.size; i++) {
-            let result= this.checkStep(arr,i,j);
-            arr = result.arr;
-            moved = result.moved;
-          }
+    let result;
+    for (let j = 0; j < this.size; j++) {
+      for (let i = 1; i < this.size; i++) {
+        result = this.checkStep(arr2, i, j, moved);
       }
-  return moved ? this.makeRandomCell(arr) : arr;
-
+    }
+    arr = rotate45(rotate45(result.arr));
+    moved = result.moved;
+    this.ifGamesOver(arr);
+    return moved ? this.makeRandomCell(arr) : arr;
   }
-  moveDown(data){
-    let arr = data.slice();
-      let arr2 = reverseY(arr);
-      let moved = false;
-      for(let j = 0; j<this.size; j++){
-          for (let i = 1; i < this.size; i++) {
-              let result= this.checkStep(arr2,i,j);
-              arr = reverseY(result.arr);
-              moved = result.moved;
-          }
-      }
-      return moved ? this.makeRandomCell(arr) : arr;
-  }
-  moveLeft(data){
+  moveLeft(data) {
     let arr = data.slice();
 
     let rotated = rotate45(arr);
-      let moved = false;
-      for(let j = 0; j<this.size; j++){
-          for (let i = 1; i < this.size; i++) {
-              let result= this.checkStep(rotated,i,j);
-              arr = rotate45(rotate45(rotate45(result.arr)));
-              moved = result.moved;
-          }
+    let moved = false;
+    let result;
+    for (let j = 0; j < this.size; j++) {
+      for (let i = 1; i < this.size; i++) {
+        result = this.checkStep(rotated, i, j, moved);
       }
-      return moved ? this.makeRandomCell(arr) : arr;
-  }
-  moveRight(data){
-    let arr = data.slice();
-      let rotated = rotate45(rotate45(rotate45(arr)));
-      let moved = false;
-      for(let j = 0; j<this.size; j++){
-          for (let i = 1; i < this.size; i++) {
-              let result= this.checkStep(rotated,i,j);
-              arr = rotate45(result.arr);
-              moved = result.moved;
-          }
-      }
-      return moved ? this.makeRandomCell(arr) : arr;
     }
+    arr = rotate45(rotate45(rotate45(result.arr)));
+    moved = result.moved;
+    this.ifGamesOver(arr);
+    return moved ? this.makeRandomCell(arr) : arr;
+  }
+  moveRight(data) {
+    let arr = data.slice();
+    let rotated = rotate45(rotate45(rotate45(arr)));
+    let moved = false;
+    let result;
+    for (let j = 0; j < this.size; j++) {
+      for (let i = 1; i < this.size; i++) {
+        result = this.checkStep(rotated, i, j, moved);
+      }
+    }
+    arr = rotate45(result.arr);
+    moved = result.moved;
+    this.ifGamesOver(arr);
+    return moved ? this.makeRandomCell(arr) : arr;
+  }
 
+  restart = () => {
+    let cells = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+    let allCells = this.makeRandomCell(this.makeRandomCell(cells));
+
+    this.setState({
+      cells: allCells,
+      latestCoordinates: {},
+      gameOver: false,
+      score: 0
+    });
+  };
 
   render() {
-   let  {cells,latestCoordinates} = this.state;
     return (
       <div className="App">
-        <Field cells={cells} latest={latestCoordinates}/>
+        <Field props={this.state} />
+        <div className="button-wrapper">
+          <button onClick={this.restart}>New Game</button>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
-
--- src/components/field.js
+++ src/components/field.js
@@ -1,34 +1,63 @@
-import React, { Component } from "react";
+import React from "react";
 
-const Field =(props) => {
-
-  const initNewField =() => {
-    let {cells} = props;
-    let {latest}=props;
-    console.log(props);
-    let divStyle={
-      webkitAnimation: 'scale-up-center 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
-      animation: 'scale-up-center 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'
-    };
-    let Tile = {
-
-    };
-
-    let allCells = cells.map((item,x) => {
-      return item.map((i,y) => {
-
-        if(latest && x===latest.i && y===latest.j){console.log('!!') }
-        // console.log(x,y, latest.i[0], latest.j[0]);
-        return <div className={'cell '+((latest && x===latest.i && y===latest.j) ? ' rt' :'')} style={divStyle}  >{i ? i : ""} </div>;
+const Field = props => {
+  let gameOver = props.props.gameOver;
+  let isWin = false;
+  let bestFromStorage = JSON.parse(localStorage.getItem("score"));
+  const initNewField = () => {
+    let cells = props.props.cells;
+    let latest = props.props.latestCoordinates;
+    let score = props.props.score;
+    let allCells = cells.map((item, x) => {
+      return item.map((i, y) => {
+        if (i === 2048) {
+          isWin = true;
+        }
+        return (
+          <div className="cell-wrapper" key={'wrap-'+x+y+i}>
+            <div
+              className={
+                "cell " +
+                (latest && x === latest.i && y === latest.j ? " new" : "") +
+                " cell_" +
+                i
+              }
+            >
+              {i ? i : ""}{" "}
+            </div>
+          </div>
+        );
       });
     });
-
-    return <div className="field">{allCells}</div>;
+    return (
+      <>
+        <div>
+          <h1 className="welcome">Welcome on board!</h1>
+          <h3 className="score">Your score: {score ? score : 0}</h3>
+          <h3 className="best-score score">
+            Best score: {bestFromStorage ? bestFromStorage : 0}{" "}
+          </h3>
+        </div>
+        <div className="field">
+          {allCells}{" "}
+          {isWin ? (
+            <div className="loose">
+              <h1>You win!</h1>
+            </div>
+          ) : (
+            ""
+          )}{" "}
+          {gameOver ? (
+            <div className="loose">
+              <h1>You loose</h1>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </>
+    );
   };
- return (
-      initNewField()
- )
-
-
+  return initNewField();
 };
-export default Field
+export default Field;
